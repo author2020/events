35414518ec0a36d0c49308fa8a374835e18da2a2
Дмитрий Печенков <do83it@gmail.com>
2024-04-12 20:57:34 +0500

    Change$SaveAdminTemplates

--- event_app/users/admin.py
+++ event_app/users/admin.py
@@ -2,5 +2,21 @@ from django.contrib import admin
 
 from .models import User, Specialization
 
-admin.site.register(User)
-admin.site.register(Specialization)
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        'email',
+        'full_name',
+        'phone',
+        'employer',
+        'occupation',
+        'experience',
+        'preferred_format',
+        'consent_personal_data_processing',
+        'consent_vacancy_data_processing',
+        'consent_random_coffee',
+        'profile_full',
+    )
+    @admin.display(description='Полное имя')
+    def full_name(self, obj):
+        return f"{obj.first_name} {obj.last_name}"
