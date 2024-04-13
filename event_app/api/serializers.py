59b206a4e227eaddac62f962ebabbd37141ed7a5
Дмитрий Печенков <do83it@gmail.com>
2024-04-13 16:09:10 +0500

    AddSpeakerValidators

--- event_app/api/serializers.py
+++ event_app/api/serializers.py
@@ -1,4 +1,6 @@
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
+from django.core.validators import RegexValidator
 
 from events.models import Event, Subevent, Speaker
 
@@ -9,6 +11,16 @@ class SpeakerSerializer(serializers.ModelSerializer):
     '''
     subevent = serializers.StringRelatedField(many=True, read_only=True)
     full_name = serializers.SerializerMethodField()
+    first_name = serializers.CharField(validators=[RegexValidator(
+        regex=r'^[a-zA-Zа-яА-Я]+$',
+        message='Имя может содержать только русские либо латинские буквы!')])
+    last_name = serializers.CharField(validators=[RegexValidator(
+        regex=r'^[a-zA-Zа-яА-Я\-]+$',
+        message='Фамилия может содержать только русские либо латинские'
+                ' буквы, а также тире!')])
+    contacts = serializers.CharField(
+        validators=[UniqueValidator(queryset=Speaker.objects.all())]
+    )
 
     class Meta:
         model = Speaker
@@ -16,6 +28,8 @@ class SpeakerSerializer(serializers.ModelSerializer):
 
     def get_full_name(self, obj):
         return f"{obj.first_name} {obj.last_name}"
+    
+
 
 
 class SubeventSerializer(serializers.ModelSerializer):
