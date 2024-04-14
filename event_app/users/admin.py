2e98636c06bb85cec85687ece8d1d9946abbeb81
Дмитрий Печенков <do83it@gmail.com>
2024-04-14 23:17:18 +0500

    Чуток улучшил

--- event_app/users/admin.py
+++ event_app/users/admin.py
@@ -5,6 +5,7 @@ from .models import User, Specialization
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = (
+        'id',
         'email',
         'full_name',
         'phone',
@@ -17,6 +18,13 @@ class UserAdmin(admin.ModelAdmin):
         'consent_random_coffee',
         'profile_full',
     )
+    list_filter = ('experience', 'consent_personal_data_processing',
+                   'preferred_format', 'consent_vacancy_data_processing',
+                   'consent_random_coffee')
+    search_fields = ('email__startswith', 'full_name')
+    ordering = ('id',)
+    list_display_links = ('email',)
+
     @admin.display(description='Полное имя')
     def full_name(self, obj):
         return f"{obj.first_name} {obj.last_name}"
